8599c5b4eb36bd36e9dc107c649fd43b48dc459d
Shivam Singh <rajshivamsingh2101@gmail.com>
2026-03-26 00:46:52 +0530

    new project

---
+++ src/test/java/pages/NavigateToSite.java
@@ -0,0 +1,19 @@
+package pages;
+
+public class NavigateToSite {
+
+    public static void main(String[] args) {
+        int num = 34324324;
+        int rev = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num = num / 10;
+
+        }
+        System.out.print(rev+" ");
+
+
+    }
+}
